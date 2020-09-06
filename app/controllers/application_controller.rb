290d54ca22518ebff5cf7e6125fdccd2d1ddd87f
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-06 11:43:36 +0900

    basic認証機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'taro' && password == '1221'
+  end
 end
