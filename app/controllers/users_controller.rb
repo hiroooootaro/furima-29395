e23bbfe728e652f0b92d50aacde4ae1977d55cfd
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-08 20:52:29 +0900

    ユーザー認証機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,2 @@
 class UsersController < ApplicationController
-  #before_action :authenticate_user!
-  #before_action :configure_permitted_parameters, if: :devise_controller?
-
-  
-  #protected
-  #def configure_permitted_parameters
-    #devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_name_kana, :birth_day,:last_name, :last_name_kana])
-  #end
 end
