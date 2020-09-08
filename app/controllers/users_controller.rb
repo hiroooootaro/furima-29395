80b491ed537a88a36db6f684bc31c5f77fb7d492
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-08 18:50:37 +0900

    bundle install

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-  def index
+  def create
   end
-
-  def new
-  end
-
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_kana_name, :birth_day,:last_name, :last_kana_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_name_kana, :birth_day,:last_name, :last_name_kana])
   end
 end
