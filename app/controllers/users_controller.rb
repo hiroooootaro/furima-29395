40d3e1529c487339528163bd6674451108799f5f
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-08 16:46:24 +0900

    user_controllerの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def new
   end
 
-  private
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_kana_name, :birth_day,:last_name, :last_kana_name])
   end
