93fee6dce695784eec88495b4d1011322a4c11bb
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-08 15:52:15 +0900

    コントローラーのセッティング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   def index
   end
 
   def new
   end
-  
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_kana_name, :birth_day,:last_name, :last_kana_name])
+  end
 end
