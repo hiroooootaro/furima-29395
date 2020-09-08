afba4162124a194c824aaf952578a32b26a7bef2
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-08 20:51:25 +0900

    viewのhoge修正&usersコントローラーの間違いを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
+
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_name_kana, :birth_day,:last_name, :last_name_kana])
+  end
+  
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
