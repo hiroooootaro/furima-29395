3ab361ca967db917ff14446573842c983b87e663
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-11 10:51:14 +0900

    itemコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,17 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create 
+    @item = items.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :area_id, :day_id, :price, :user)
   end
 end
