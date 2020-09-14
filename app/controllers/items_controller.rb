3bfa1311619a4930556501fc9b9787396a467118
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-14 19:37:00 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,except:[:index,:show]
   def index
+    @items = Item.all
     @item = Item.order("created_at DESC")
-    #@items = Item.find(params[:id])
   end
 
   def new
@@ -16,7 +17,8 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+  def show
+  end
 
   private
 
