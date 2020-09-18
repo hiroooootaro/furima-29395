3ebe9b81b173893d5cc42fd9dc28d5d4a2c319c7
hiroooootaro <taro.hirotaro@icloud.com>
2020-09-18 17:29:03 +0900

    修正4

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -3,12 +3,14 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :name, :user_id, :item_id, :postal, :prefecture_id, :city, :address,:building_name, :phone_number, :authenticity_token, :token, :order_id
 
+
+  VALID_ADDRESS_REGEX = /\A[ぁ-んァ-ン一-龥]/
   with_options presence: true do
     validates :phone_number, format: {with:/\A\d{,11}\z/}
-    validates :address, format: {with:/\A[ぁ-んァ-ン一-龥]/}
-    validates :postal, format: { with:/\A\d{3}[-]\d{4}\z/}
-    validates :prefecture_id, numericality: {other_than: 0, message: 'Select'}
-    validates :city, format: {with:/\A[ぁ-んァ-ン一-龥]/}
+    validates :address, format: {with: VALID_ADDRESS_REGEX}
+    validates :postal, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: {other_than: 0}
+    validates :city, format: {with: VALID_ADDRESS_REGEX}
     validates :user_id
     validates :item_id
   end
